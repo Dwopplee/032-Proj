6b4bb54ddc2d35ab8f4a59c3471b390dd87e224a
Dwopplee <charlesparkinson14@gmail.com>
2019-10-23 12:03:55 -0700

    Update poxExps function, add Test.R
    * Things are getting slightly less gross

--- Analyze.R
+++ Analyze.R
@@ -1,5 +1,3 @@
-rm(list=ls(all=TRUE))
-
 source('Setup.R')
 
 splitX = SplitSet(0.8, X, y)
@@ -8,6 +6,11 @@ XTest = splitX$testData
 yTrain = splitX$trainTarget
 yTest = splitX$testTarget
 
+
+# Absolute worst a model should be
+baseModel = lm(yTrain ~ 1, data = XTrain)
+baseError = AbsError(baseModel, XTest, yTest)
+
 # TODO: make some functions:
 #   1 iterates through positive exponents until model gets worse
 #   2 iterates through negative exponents until model gets worse
@@ -15,24 +18,29 @@ yTest = splitX$testTarget
 # Test models exclusively with test set. use same training/test set for all models
 # (or something)
 
-posExps = function(predictor, data) {
-  # This is disgusting
-  oldError = 101000
-  newError = 100000
+# TODO: Make this function take inputs (predictor and target)
+# This function outputs slightly different things based on what the training and test sets are
+#   May want to run this multiple times, if possible.
+posExps = function(predictor) {
+  oldError = Inf
+  newError = baseError
   maxPow = 0
-  while (newError < oldError) {
+  # `> 0.01` is there as a threshold to reduce variablity
+  # Also means that we favor simpler models over models with slightly less error
+  # May be interesting to test this with MSE rather than MAE
+  while ((newError < oldError) && (abs(oldError - newError) > 0.01)) {
     maxPow = maxPow + 1
     
-    pows = paste("I(population**",c(1:maxPow),")",sep='')
+    pows = paste("I(", predictor, "**",c(1:maxPow),")",sep='')
     f = as.formula(paste("yTrain ~ ", paste(pows, collapse="+")))
     model = lm(f, data = XTrain)
     
     oldError = newError
     newError = AbsError(model, XTest, yTest)
-    print(newError)
+    # cat(maxPow, ":", oldError, newError, '\n')
   }
   
-  return(maxPow)
+  return(maxPow-1)
 }
 
-print(posExps("predictor", "data"))
+return(posExps("population"))
