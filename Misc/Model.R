f9ca0dfedc9ead9a1dcca2a3a865dc5a7fdab79a
Dwopplee <charlesparkinson14@gmail.com>
2019-10-24 13:04:04 -0700

    Add some stuff from Oct24 lecture
    * nothing really helpful, hopefully i learned something tho?

--- Misc/Model.R
+++ Misc/Model.R
@@ -30,10 +30,35 @@ ytest = y[-trainrows]
 
 trainModel = lm(ytrain ~ ., data = Xtrain)
 
-print(summary(trainModel))
+# print(summary(trainModel))
 plot(ytest, predict(trainModel, newdata = Xtest), xlim = c(0, 1), ylim = c(0, 1))
 abline(0, 1)
 
 # Something about balancing data set. is this doable with continuous model?
 # Forces favoring of one side or the other.
 #   Actually this is our question about high violence rates, sort of.
+
+
+# He moved on to continuous data sets (!)
+# smaller set, easier to analyze by hand
+
+Correlation = function(predictor) {
+  cor(predictor, y)  
+}
+
+results = sapply(X[, c(1:122)], Correlation)
+# Have high linear relationship
+print(which(abs(results)>0.5))
+
+# if you have classification data points, then convert to binary variable
+# called contrast coding
+# maybe relevant to:
+#   LemasGangUnitDeploy
+# but maybe not; already semi-binary
+
+# for testing accuracy of model, qualitatively
+# plot(density(prediction-actual))
+# or
+# plot(prediction, actual)
+# and
+# abline(0, 1)
