253d98edc72de423ac538a9387a198e9e39df262
Dwopplee <charlesparkinson14@gmail.com>
2019-10-23 17:35:18 -0700

    Finish posExp function
    * Copied it and made minor changes for negative counterpart
      * Needs to work for when data set contains zeros
    * Still needs implementation
    * Test.R runs file 1000 times, generates histogram of all maxPows
      * Have to substitute relevant function name in - wont currently work

--- Analyze.R
+++ Analyze.R
@@ -12,35 +12,67 @@ baseModel = lm(yTrain ~ 1, data = XTrain)
 baseError = AbsError(baseModel, XTest, yTest)
 
 # TODO: make some functions:
-#   1 iterates through positive exponents until model gets worse
+#   1 iterates through positive exponents until model gets worse (done?)
 #   2 iterates through negative exponents until model gets worse
 #   3 compares both models (adds them together?) then chooses best one
 # Test models exclusively with test set. use same training/test set for all models
 # (or something)
 
-# TODO: Make this function take inputs (predictor and target)
-# This function outputs slightly different things based on what the training and test sets are
-#   May want to run this multiple times, if possible.
-posExps = function(predictor) {
+# Increases maximum exponent until error increases
+#   predictor   a string containing the name of column to model
+#   trainData   data used for modeling; must contain predictor
+#   testData    data used for testing; must contain predictor
+#   trainTarget data of target variable used for modeling
+#   testTarget  data of target variable used for testing
+# Returns the highest exponent reached before error increased
+# TODO: This function outputs slightly different things based on what the training and test sets are
+#           May want to run this multiple times, if possible. Maybe use median value?
+posExps = function(predictor, trainData, testData, trainTarget, testTarget) {
   oldError = Inf
   newError = baseError
   maxPow = 0
   # `> 0.01` is there as a threshold to reduce variablity
   # Also means that we favor simpler models over models with slightly less error
-  # May be interesting to test this with MSE rather than MAE
+  # May be interesting to test this with MSE rather than MAE; would have to change threshold
   while ((newError < oldError) && (abs(oldError - newError) > 0.01)) {
     maxPow = maxPow + 1
     
-    pows = paste("I(", predictor, "**",c(1:maxPow),")",sep='')
-    f = as.formula(paste("yTrain ~ ", paste(pows, collapse="+")))
-    model = lm(f, data = XTrain)
+    pows = paste("I(", predictor, "**", c(1:maxPow), ")", sep='')
+    f = as.formula(paste("trainTarget ~ ", paste(pows, collapse = "+")))
+    model = lm(f, data = trainData)
     
     oldError = newError
-    newError = AbsError(model, XTest, yTest)
-    # cat(maxPow, ":", oldError, newError, '\n')
+    newError = AbsError(model, testData, testTarget)
   }
   
-  return(maxPow-1)
+  return(maxPow - 1)
 }
 
-return(posExps("population"))
+# TODO: document
+# TODO: Fix case where we divide by zero (I think that's the problem):
+# ```Error in lm.fit(x, y, offset = offset, singular.ok = singular.ok, ...) : 
+#       NA/NaN/Inf in 'x'```
+# This function outputs slightly different things based on what the training and test sets are
+#   May want to run this multiple times, if possible. Maybe use median value?
+negExps = function(predictor, trainData, testData, trainTarget, testTarget) {
+  oldError = Inf
+  newError = baseError
+  minPow = 0
+  # `> 0.01` is there as a threshold to reduce variablity
+  # Also means that we favor simpler models over models with slightly less error
+  # May be interesting to test this with MSE rather than MAE; would have to change threshold
+  while ((newError < oldError) && (abs(oldError - newError) > 0.01)) {
+    minPow = minPow - 1
+    
+    pows = paste("I(", predictor, "**", c(-1:minPow), ")", sep='')
+    f = as.formula(paste("trainTarget ~ ", paste(pows, collapse = "+")))
+    model = lm(f, data = trainData)
+    
+    oldError = newError
+    newError = AbsError(model, testData, testTarget)
+  }
+  
+  return(minPow + 1)
+}
+
+return(negExps("population", XTrain, XTest, yTrain, yTest))
--- Test/Test.R
+++ Test/Test.R
@@ -2,7 +2,7 @@ rm(list=ls(all=TRUE))
 
 test = function(i) {
   source('Analyze.R')
-  return(posExps("population"))
+  return(negExps("population", XTrain, XTest, yTrain, yTest))
 }
 
 results = sapply(c(1:1000), test)
