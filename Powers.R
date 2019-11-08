5232f3e7ac68354e08ab917be36a30a337b33d66
Dwopplee <charlesparkinson14@gmail.com>
2019-11-08 09:24:41 -0800

    Now reads off the right data set to make model
    * Sometimes produces NA coefs, in this case model sucks
    * Will have to merge with wip *sigh*

--- Powers.R
+++ Powers.R
@@ -1,4 +1,15 @@
-rm(list=ls(all=TRUE))
+rm(list = ls(all = TRUE))
+
+# This is the first iteration of the model.
+
+# We find the "optimal" polynomial relationship for each predictor with our
+# dependent variable by repeatedly splitting our data set into training and test
+# sets, then iterating up through higher degree polynomials until the absolute
+# error in the training set increases. We do this 100 times in an attempt to
+# remove the effect of the random sampling of the train/test split.
+
+# We then create a model by adding together all the polynomials with degree of
+# at least 1.
 
 source('SetupPows.R')
 
@@ -8,8 +19,14 @@ source('SetupPows.R')
 #   target  the numeric (vector) of target variables
 SampleExps = function(pct, data, target) {
   split = SplitSet(pct, data, target)
-  exps =  sapply(colnames(X), PosExps, split$trainData, split$testData,
-          split$trainTarget, split$testTarget)
+  exps =  sapply(
+    colnames(X),
+    PosExps,
+    split$trainData,
+    split$testData,
+    split$trainTarget,
+    split$testTarget
+  )
   return(exps)
 }
 
@@ -20,16 +37,25 @@ exps = replicate(100, SampleExps(0.8, X, y))
 modes = sapply(exps[, ncol(exps)], Mode)
 
 # We don't want predictors that are best as constants
-Xmodel = X[, -which(modes==0)]
-modes = modes[-which(modes==0)]
+Xmodel = X[, -which(modes == 0)]
+modes = modes[-which(modes == 0)]
 
 # Generate and plot a model with all predictors raised to generated powers
 
-splitX = SplitSet(0.8, X, y)
+splitX = SplitSet(0.8, Xmodel, y)
 
-powForm = paste(mapply(ExpFormula, colnames(X), modes), collapse = "+")
+powForm = paste(mapply(ExpFormula, colnames(Xmodel), modes), collapse = "+")
 f = as.formula(paste("splitX$trainTarget ~ ", powForm))
 model = lm(f, data = splitX$trainData)
 
 plot(predict(model, newdata = splitX$testData), splitX$testTarget)
 abline(0, 1)
+
+cat("Model 0 error:", AbsError(model, splitX$testData, splitX$testTarget), '\n')
+
+# This model doesn't quite suck (!), but we can (hopefully) do better.
+# It also has limited impact on our research question -- the impact of variables
+# with high dependence on other variables may be diminished
+# Notable issues:
+#   High saturation: we predict negative crime, and crime greater than 1
+#   Error increases as crime (predicted and actual) increases
